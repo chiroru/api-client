6572f995c9aeedf476520c2bbb6ed872918240dc
chiroru <chiroru_0130@yahoo.co.jp>
2013-12-18 00:44:52 +0900

    追加実装

---
+++ src/validation.py
@@ -0,0 +1,26 @@
+'''
+Created on 2013/12/18
+
+@author: smts1008@outlook.com
+'''
+
+class Validation(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
+    @classmethod
+    def isNumeric(self, suspect):
+        if suspect == 1:
+            return True
+
+    @staticmethod
+    def isTel(suspect):
+        if suspect == 2:
+            return True
